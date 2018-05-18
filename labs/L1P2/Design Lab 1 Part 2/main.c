e4be83fbdc657fbc915a7cae9d385ccb92bdecea
Shamus <shamusmacdonald@gmail.com>
2018-05-18 10:45:59 -0300

    Lab 2 bug fix

--- labs/L1P2/Design Lab 1 Part 2/main.c
+++ labs/L1P2/Design Lab 1 Part 2/main.c
@@ -12,6 +12,7 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+#define TIMER_SPEED	(F_CPU/8)
 /*
 	PIN Change ISR for PC0.
 	Measures the frequency and duty cycle of PWM waveforms and outputs the values
@@ -25,28 +26,38 @@ ISR(PCINT2_vect)
 	static uint16_t t_high;
 	static uint16_t t_duty;
 	static uint16_t duty;
-	static uint8_t falling;
+	static uint8_t falling = 0;
 	static uint8_t first_rise = 1;
 	static uint16_t freq;
 		
 	if(PINC & 0x01) {
 		if(first_rise) {
+		/* This handles the first rise of a PWM */
 			t1 = ((uint16_t)TCNT1L) | ((uint16_t)TCNT1H<<8);
 			first_rise = 0;
 			falling = 1;
 		}
-		else {
+		else if(!falling) {
+		/* this handles the second rise of a period of a PWM */
 			t2 = ((uint16_t)TCNT1L) | ((uint16_t)TCNT1H<<8);
 			delta_t = t2 - t1;
 			t_duty = t_high - t1;
 			duty = (uint16_t)(((float)t_duty/delta_t)*100.);
-			freq = (uint16_t)(F_CPU/delta_t);
+			freq = (uint16_t)(TIMER_SPEED/delta_t);
 			printf("\nt1 = %u\nt2 = %u\ndelta_t = %u\nt_high = %u\nt_duty = %u\nduty = %u\nfreq = %u\n",t1,t2,delta_t,t_high,t_duty,duty,freq);
 			first_rise = 1;
-			_delay_ms(1000);
+			_delay_ms(2500);
+		}
+		else {
+		/*	This handles if an interrupt is missed
+			This will reset the pulse we are measuring */
+			first_rise = 1;
+			falling = 0;
+			
 		}
 	}
 	else if(falling){
+	/* This handles the falling edge of the PWM period */
 		t_high = ((uint16_t)TCNT1L) | ((uint16_t)TCNT1H<<8);
 		falling = 0;
 	}
@@ -64,13 +75,15 @@ int main(void)
 /*	Set one wave to have 25% duty cycle and one to have 75% duty cycle */
 	OCR0A = 64;
 	OCR0B = 191;
-/*	Enable TIMER1 */
-	TCCR1B = (1<<CS10);
+/*	Enable TIMER1 with prescale factor 8 */
+	TCCR1B = (1<<CS11);
 /*	Enable PCINT on PC0 */
 	PCICR |= (1<<PCIE2);			
 	PCMSK2 |= (1<<PCINT16);
 	sei(); 
-		
+	
+	_delay_ms(1000);
+	
 	while(1)
 	{
 	}
