defb14e6b5008f243f31bbdb35214f2dacb46a10
Shamus MacDonald <shamusmacdonald@gmail.com>
2018-05-24 22:19:21 -0300

    Update main.c

--- labs/L1P1/Design Lab 1/main.c
+++ labs/L1P1/Design Lab 1/main.c
@@ -7,20 +7,20 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include "uart.h"
 
 ISR(PCINT2_vect)
 {
 	if(PINC & 0x01)
 	{
-		//Configure Fast PWM Mode
+	/*	Configure Fast PWM Mode if PC0 high */
 		TCCR0A |= (1<<WGM00) | (1<<WGM01) | (1<<COM0B1) | (1<<COM0A1) ;
 		TCCR0B |= (1<<CS01) ;
-		//Set one wave to have 25% duty cycle and one to have 75% duty cycle
+	/*	Set one wave to have 25% duty cycle and one to have 75% duty cycle */
 		OCR0A = 64;
 		OCR0B = 192;
 	}
 	else {
+	/* 	Turn off PWM if PC0 low */
 		TCCR0A &= (0<<WGM00) | (0<<WGM01) | (0<<COM0B1) | (0<<COM0A1) ;
 		TCCR0B &= (0<<CS01) ;
 	}
@@ -28,17 +28,17 @@ ISR(PCINT2_vect)
 
 int main(void)
 {
-	init_uart();
-	//Set Data Direction Bits for Output
+/*	Set Data Direction Bits for Output */
 	DDRB |= (1<<PB3) | (1<<PB4) ;
 	DDRC &= (0<<PC0);
-
+/*	Enable Pin change interrupts on PC0 */
 	PCICR |= (1<<PCIE2);
 	PCMSK2 |= (1<<PCINT16);
-	sei(); // enables global interrupts
+	sei(); 
 	
 	while(1)
 	{
+	/* Loop until interrupt */
 	}
 }
 
